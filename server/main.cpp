b1afcfedfaa03647cd0041d76da6019e314eb1d9
Edgar Catacora <catacora@shernet.sheridancollege.ca>
2018-01-30 21:25:48 -0500

    fix project include configuration

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,27 @@
-int main()
-{
-	return -1;
+
+#include "RakNetServer.h"
+
+#include <RakSleep.h>
+
+#include <Windows.h>
+
+#include <iostream>
+
+
+int main(int argc, char **argv) {
+	RakNetServer server;
+	server.Start(1288, 10);
+	//Sleep(1000);
+	//server.Shutdown(1000);
+	//Make sure the server block duration is allowed to run to completion.
+	//Sleep(1500);
+
+	while (true) {
+		RakNet::Packet *packet = server.Receive();
+		if (packet != NULL) {
+			server.DeallocatePacket(packet);
+		}
+		RakSleep(50);
+	}
+
 }
